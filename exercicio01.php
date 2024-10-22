fe665f0e6cef86099dbe5b63a187f66c2adbdbc2
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-10-22 15:04:13 -0300

    correçao associado e indesejado

--- exercicio01.php
+++ exercicio01.php
@@ -9,44 +9,70 @@
 </head>
 
 <body>
-    <h1>Exercicio PHP</h1>
+    <div class="container">
+        <h1>Exercicio PHP</h1>
 
-    <?php
+        <?php
 
 
-    /* Array ASSOCIATIVO */
-    $dados1 = [
-        "nome_usuario" => "chapolin.colorado",
-        "senha" => 123,
-        "idade" => 30,
-        "cidade" => "São Paulo",
-        "telefone" => ["(11)999-5488", "(11)924-5468"]
+        /* Array ASSOCIATIVO */
+        $dados1 = [
+            "nome_usuario" => "chapolin.colorado",
+            "senha" => 123,
+            "idade" => 30,
+            "cidade" => "São Paulo",
+            "telefone" => ["(11)999-5488", "(11)924-5468"]
 
-    ];
+        ];
 
-    /* Array NUMÉRICO/INDEXADO */
-    $dados2 = [
-        'chapolin.colorado',
-        '123teste',
-        30,
-        'São Paulo',
-        ['(11)999-5478", "(11)924-5478']
+        /* Array NUMÉRICO/INDEXADO */
+        $dados2 = [
+            'chapolin.colorado',
+            '123teste',
+            30,
+            'São Paulo',
+            ["(11)999-5478", "(11)924-5478"]
 
-    ]
-    ?>
+        ]
+        ?>
 
-    <hr>
+        <hr>
 
-   <!--  Array ASSOCIATIVO   -->
-    <ol>   
-        <li>Nome de Usuário : <?= $dados1["nome_usuario"] ?></li>
-        <li>Senha : <?= $dados1["senha"] ?></li>
-        <li>Idade : <?= $dados1["idade"] ?></li>
-        <li>Cidade : <?= $dados1["cidade"] ?></li>
-        <li>1 Telefone : <?= $dados1["telefone"][0] ?></li>
-        <li>2 Telefone : <?= $dados1["telefone"][1] ?></li>
-    </ol>
+        <!--  Array UMÉRICO/INDEXADO   -->
+        <ol>
+            <li>Nome de Usuário : <?= $dados1["nome_usuario"] ?></li>
+            <li>Senha : <?= $dados1["senha"] ?></li>
+            <li>Idade : <?= $dados1["idade"] ?></li>
+            <li>Cidade : <?= $dados1["cidade"] ?></li>
+            <li>1 Telefone : <?= $dados1["telefone"][0] ?></li>
+            <li>2 Telefone : <?= $dados1["telefone"][1] ?></li>
+        </ol>
 
+
+        <hr>
+        <h1>Correção do Exercícios</h1>
+        <br>
+        <br>
+        <h2>Versão de saida usando arrays NUMÉRICO/INDEXADO</h2>
+        <ol>
+            <li>Nome: <?= $dados2[0] ?></li>
+            <li>Idade: <?= $dados2[2] ?></li>
+            <li>Cidade: <?= $dados2[3] ?></li>
+            <li>Celular: <?= $dados2[4][0] ?></li>
+            <li>Celular: <?= $dados2[4][1] ?></li>
+        </ol>
+
+        <h2>Versão de saida usando arrays ASSOCIATIVO</h2>
+
+        <ol>
+            <li>Nome de Usuário : <?= $dados1["nome_usuario"] ?></li>
+            <li>Senha : <?= $dados1["senha"] ?></li>
+            <li>Idade : <?= $dados1["idade"] ?></li>
+            <li>Cidade : <?= $dados1["cidade"] ?></li>
+            <li>1 Telefone : <?= $dados1["telefone"][0] ?></li>
+            <li>2 Telefone : <?= $dados1["telefone"][1] ?></li>
+        </ol>
+    </div>
 
 </body>
 
