e1d0387e0b49eaf6549be87efcd2c082607dd808
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-10-29 16:42:58 -0300

    Create exercicio03-com-bootstrap.php

---
+++ exercicio03-com-bootstrap.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio - 03</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .list-group-item:nth-child(odd) {
+            /*Ímpar */
+            color: blue;
+
+        }
+
+        .list-group-item:nth-child(even) {
+            /* Par*/
+            color: red;
+
+        }
+    </style>
+</head>
+
+
+<body>
+    <div class="container">
+
+        <h1>Exercício 03</h1>
+
+        <?php
+        $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        ?>
+
+
+        <!-- Utilizando a tag  For  👇-->
+        <!-- 
+    <ol >
+        <?php
+        $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        for ($i = 0; $i < count($meses); $i++) { ?>
+
+        <li>
+            <?= $meses[$i] ?>
+        </li>
+
+        <?php
+        }
+        ?>
+        
+    </ol> -->
+
+        <!-- Utilizando a variavel Foreach 👇 -->
+
+        <ol class="lis-group list-group-numbered">
+
+            <?php foreach ($meses as $mes) { ?>
+
+                <li class="list-group-item"><?= $mes ?></li>
+
+            <?php } ?>
+
+        </ol>
+    </div>
+</body>
+
+</html>
