52476d92475b3cc23075f50e96ddc2aeddf278b4
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-10-30 17:29:23 -0300

    Dupla validaçao

--- processamento.php
+++ processamento.php
@@ -1,33 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
+ 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Processamento</title>
 </head>
-
+ 
 <body>
     <h1>Processamento dos dados vindos do formulário HTML</h1>
     <hr>
-
+ 
     <?php
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $nascimento = $_POST["nascimento"];
     $mensagem = $_POST["mensagem"];
+    // Se um dos campos obrigatórios estiver vazio
+    if (empty($nome) || empty($email)) {
     ?>
-
-    <h2>Dados recebidos</h2>
-    <ul>
-        <li>nome: <?=$nome ?></li>
-        <li>E-mail: <?=$email ?></li>
-        <li>Data de nacimento: <?=$nascimento ?></li>
-        <li>mensagem: <?=$mensagem ?></li>
-    </ul>
-
-
-
+        <p style="color: red">Tem que preencher !!</p>
+    <?php
+    } else {
+    ?>
+        <h2>Dados Recebidos</h2>
+        <ul>
+            <li>Nome: <?= $nome ?></li>
+            <li>Email: <?= $email ?></li>
+ 
+            <!-- Verificando se os campos nascimento e mensagem NÃO estão vazios, o uso do operador ! para inverter a lógica da função empty() -->
+            <?php if (!empty($nascimento)) { ?>
+                <li>Data do Nascimento: <?= $nascimento ?></li>
+            <?php } ?>
+ 
+            <?php if (!empty($mensagem)) { ?>
+                <li>Mensagem: <?= $mensagem ?></li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+ 
+ 
 </body>
-
+ 
 </html>
